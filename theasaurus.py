2662820bb2d5c2e67cce081ee0cb3d440ec5f0f4
avisionh <a_vision@hotmail.co.uk>
2020-11-10 16:28:18 +0000

    Call wrapper around synonyms.com API
    This is to try an alternative method to get synonyms.

--- theasaurus.py
+++ theasaurus.py
@@ -1,7 +1,12 @@
 from nltk.corpus import wordnet
+from PyDictionary import PyDictionary
 
 # use wordnet to get synonyms of finance - really poor!
 synonyms = []
 for word in wordnet.synsets('finance'):
     for synonym in word.lemma_names():
         synonyms.append(synonym)
+
+# call API to get synonyms - slightly better
+dictionary = PyDictionary()
+print(dictionary.synonym('finance'))

59e0d8b3373504d1b88a4d77943556f72954ac3f
whojammyflip <jg.singleton@gmail.com>
2020-12-18 18:12:45 +0000

    deep graph infomax investigation

---
+++ notebooks/dgi_annoy.py
@@ -0,0 +1,190 @@
+'''
+use Approximate Nearest Neighbours to see whether the doc vectors work
+requires the Spotify Annoy package from Conda
+depends on previous creation of two data frames:
+- embeddings_use_large_2000_df.csv
+- text_use_large_2000_df.csv
+or whatever you have chosen to call them in the previous step of creating embedding vectors
+it makes sense to create various differently named flavours of embedding vectors and adding
+them to the settings json beneath, so that you can easily compare different vectors
+'''
+
+import pandas as pd
+import numpy as np
+import os
+from annoy import AnnoyIndex
+
+os.chdir('/home/james/Documents/gds_nlp/govuk-knowledge-graph/data')
+
+
+def get_cosine_from_similarity(similarity, dp=4):
+    '''
+    converts the similarity distance metric into a cosine angle
+    '''
+    cosine_angle = 1 - (similarity**2) / 2
+    return cosine_angle
+
+
+def cos_sim(a, b):
+    """
+    Takes 2 vectors a, b and returns the cosine similarity
+    """
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
+
+
+# json of settings for the testing
+settings = {'dilbert': {'embeddings': 'embeddings_distilbert_base_df.csv',
+                        'text': 'text_distilbert_base_df.csv',
+                        'embedding_dim': 768,
+                        'ann_index': 'sbert.ann'},
+            'use_large': {'embeddings': 'embeddings_use_large_df.csv',
+                          'text': 'text_use_large_df.csv',
+                          'embedding_dim': 512,
+                          'ann_index': 'use.ann'},
+            'use_2000': {'embeddings': 'embeddings_use_large_2000_df.csv',
+                         'text': 'text_use_large_2000_df.csv',
+                         'embedding_dim': 512,
+                         'ann_index': 'use_2000.ann'},
+            'dgi_use_128': {'embeddings': 'dgi_embeddings_df.csv',
+                            'text': 'text_use_large_2000_df.csv',
+                            'embedding_dim': 128,
+                            'ann_index': 'dgi_use_128.ann'}}
+
+# select which embeddings you are going to analyse
+embedding_type = 'dgi_use_128'
+
+# pass in the settings above for importing the embeddings data and matching text
+embeddings_df = pd.read_csv(settings[embedding_type]['embeddings'])
+text_df = pd.read_csv(settings[embedding_type]['text'])
+embeddings = np.asarray(embeddings_df.iloc[:, :settings[embedding_type]['embedding_dim']])
+content_id = embeddings_df['content_id'].to_list()
+
+# build the Annoy Index, f is the length of embedding vector to be used
+# f is the length of the vectors, aka 'embedding dimension'
+f = settings[embedding_type]['embedding_dim']
+# declare an empty index which is going to be based on cosine similarity, aka 'angular'
+t = AnnoyIndex(f, 'angular')
+# add embedding vectors into the index, converting the values into a list before hand
+for i in range(embeddings_df.shape[0]):
+    v = [x for x in embeddings[i, :]]
+    t.add_item(i, v)
+
+# number of trees determines how refined the groupings are, in practice, it makes little
+# difference to performance, in theory, it increases performance with greater number of trees
+# you can specify thousands of trees -  no advantage seen in doing this though
+t.build(10)
+t.save(settings[embedding_type]['ann_index'])
+
+# load a previously trained Annoy Index (the one you saved two lines up)
+u = AnnoyIndex(f, 'angular')
+u.load(settings[embedding_type]['ann_index'])
+# u.unload()
+
+
+def print_cosine_and_texts(text_idx, max_length_string=1000, verbose=True):
+    '''
+    function for printing out details of query document and the best match
+    '''
+    results = np.array(u.get_nns_by_item(text_idx, 2, include_distances=True))
+    cosine_angle = get_cosine_from_similarity(results[1, 1])
+    if verbose:
+        print('cosine angle: ' + '%s' % float('%.2g' % cosine_angle))
+        print("----")
+        print('Index: ' + str(text_idx))
+        print("----")
+        print('query doc: ' + text_df['content_id'][results[0, 0]])
+        print('date: ' + text_df['first_published_at'][results[0, 0]][:10])
+        print("----")
+        print(text_df['doc_text'][results[0, 0]][:max_length_string])
+        print("----")
+        print('best match: ' + text_df['content_id'][results[0, 1]])
+        print('date: ' + text_df['first_published_at'][results[0, 1]][:10])
+        print("----")
+        print(text_df['doc_text'][results[0, 1]][:max_length_string])
+    else:
+        return(cosine_angle)
+
+
+# pick a random document, and search the index, printing to screen
+print_cosine_and_texts(np.random.randint(0, 1000))
+# this is really how you can compare the ability of different embeddings
+print_cosine_and_texts(3779, max_length_string=10000)
+
+# collect a list of potentially interesting documents, inspecting results
+list_interesting_indices = []
+n = 1000
+while n > 0:
+    n -= 1
+    text_idx = np.random.randint(0, 30000)
+    cosine_angle = print_cosine_and_texts(text_idx, verbose=False)
+    if cosine_angle > 0.9 and cosine_angle < 0.95:
+        list_interesting_indices.append(text_idx)
+
+# check count of collected docs which match your criteria above
+len(list_interesting_indices)
+
+# set counter
+i = 0
+# if you highlight the two lines beneath, and hit 'shift + enter' in vs code
+# you can manually inspect the results
+print_cosine_and_texts(list_interesting_indices[i])
+i += 1
+
+
+# LOOKING AT GUIDANCE ON VIRUS CONTENT
+# this is just an illustration of finding content based on keywords and then looking for similar
+# content
+
+# search by date and content type:
+doc_types = ['press_release', 'news_story', 'speech', 'world_news_story', 'guidance']
+doc_types = ['guidance']
+doc_mask = text_df['document_type'].isin(doc_types)
+date_mask = text_df['first_published_at'].str[:4].fillna('2000').astype(int) >= 2020
+text_mask = text_df['doc_text'].str.lower().str.contains('virus')
+content_mask = date_mask & doc_mask & text_mask
+cols_keep = ['document_type', 'content_id', 'first_published_at', 'doc_text']
+subset_text_df = text_df.loc[content_mask, cols_keep].copy()
+subset_text_df.shape
+
+collected_guidance_ids = []
+for i in subset_text_df.index.to_list():
+    results = np.array(u.get_nns_by_item(i, 2, include_distances=True))
+    cosine_angle = get_cosine_from_similarity(results[1, 1])
+    if cosine_angle > 0.8:
+        collected_guidance_ids.append(i)
+
+print_cosine_and_texts(96897)
+i = 0
+print_cosine_and_texts(collected_guidance_ids[i])
+i += 1
+
+
+# search by vector
+# this illustrates how you can search for similar documents, based on any text
+# being convered into an embedding schema
+# requires the loading of the embedding model, which can take time to load, as its 1GB
+# flake complains about the import of libraries here, but it will add to the run time of the script
+# happy for this to be chopped and moved etc
+'''
+from universal_sentence_encoder import document_embedding
+
+test_text = ['Britain will roll out COVID-19 vaccinations when they are ready based on clinical advice about who \
+    should be prioritised, health minister Matt Hancock said on Monday, after a report that half the population \
+    could miss out on the jabs.',
+    'Asked about comments by the chair of the government vaccine taskforce to the Financial Times that vaccines \
+    would probably only be available to less than half the UK population, Hancock said the taskforce had done \
+    good work in procuring vaccines but that deployment was his department responsibility.',
+    '“We will take the advice on the deployment of the vaccine, based on clinical advice from the Joint \
+    Committee on vaccinations and immunizations,” Hancock told parliament.']
+
+embedding = document_embedding(test_text)
+results = np.array(u.get_nns_by_vector(embedding, 4, include_distances=True))
+
+get_cosine_from_similarity(results[1,0])
+text_df['doc_text'][results[0,0]]
+'''
+
+# end code
---
+++ notebooks/initial_connection.py
@@ -0,0 +1,48 @@
+# aim: import data from neo4j into environment
+# this can be extended as at present, its only going to retrieve the relationship triple store data
+
+from py2neo import Graph
+import pickle
+import os
+
+DATA_DIR = '/home/james/Documents/gds_nlp/govuk-entity-personalisation/data'
+
+gov_graph = Graph(scheme="https",
+                  host="knowledge-graph.integration.govuk.digital",
+                  port=7473,
+                  secure=True,
+                  auth=('neo4j', '################'))
+
+
+# test the connection
+limit_1_result = gov_graph.run("MATCH (n:COVID_19) RETURN n LIMIT 1").data()
+limit_1_result[0]['n']
+
+# count of nodes
+len(gov_graph.nodes)
+
+# look at some node properties
+type(limit_1_result[0]['n'])
+limit_1_result[0]['n'].labels
+limit_1_result[0]['n']['contentID']
+limit_1_result[0]['n'].keys()
+limit_1_result[0]['n']['public_updated_at']
+limit_1_result[0]['n']['updated_at']
+limit_1_result[0]['n']['documentType']
+limit_1_result[0]['n']['contentID']
+limit_1_result[0]['n']['name']
+limit_1_result[0]['n']['description']
+limit_1_result[0]['n']['first_published_at']
+limit_1_result[0]['n']['text']
+limit_1_result[0]['n']['publishing_app']
+limit_1_result[0]['n']['title']
+
+# retrieve the edge weights, triple store of: source, target, weight
+# here we are restricting analysis to nodes with the Label "Cid"
+result = gov_graph.run("MATCH (m:Cid) -[r:USER_MOVEMENT]-> (n:Cid) RETURN m.contentID, n.contentID, r.weight").data()
+len(result)
+
+filehandler = open(os.path.join(DATA_DIR, 'relationship_weights.bin'), 'wb')
+pickle.dump(result, filehandler)
+
+# end code

44b67f89a382dfdaa2b454640e0083974a3ad94a
avisionh <a_vision@hotmail.co.uk>
2020-12-29 15:41:08 +0000

    Create function to extract linguistic feature and turn to df
    This is so we can plot most common POS.

--- src/utils/helper_intent_uis.py
+++ src/utils/helper_intent_uis.py
@@ -1,4 +1,6 @@
+from typing import Union
 import spacy
+import pandas as pd
 
 
 nlp = spacy.load('en_core_web_sm')
@@ -32,3 +34,48 @@ def get_linguistic_features(txt: str) -> dict:
     dict_linguistic = dict(zip(list_linguistic_key, list_linguistic_value))
 
     return dict_linguistic
+
+
+def get_dict_to_df(txt: Union[str, list], linguistic_feature: str) -> pd.DataFrame:
+    """
+    Extracts a linguistic feature from list of strings and transforms to dataframe.
+
+
+    :param txt: String or list of strings to extract linguistic features from.
+    :param linguistic_feature: String of the linguistic feature such as POS, dep or tag that you want to extract.
+                               The linguistic features you can extract are:
+                                    - pos: parts of speech,
+                                    - tag: detailed POS-tag,
+                                    - dep: relation between tokens,
+                                    - lemma: base form of word, and
+                                    - stop: is token part of a stop list
+
+    :return: Dataframe of original strings and their linguistic features in 'long' format.
+    """
+    try:
+        if isinstance(txt, str):
+            txt = [txt]
+            return get_dict_to_df(txt=txt, linguistic_feature=linguistic_feature)
+        else:
+            feature_all = [get_linguistic_features(txt) for txt in txt]
+            feature = [txt.get(linguistic_feature) for txt in feature_all]
+
+            # store together as dictionary
+            intent_feature = dict(zip(txt, feature))
+
+            # convert to dataframe
+            df_feature = pd.DataFrame.from_dict(data=intent_feature,
+                                                orient='index').reset_index()
+            df_feature = df_feature.melt(id_vars="index",
+                                         var_name="word_position",
+                                         value_name=linguistic_feature)
+            df_feature["word_position"] = df_feature["word_position"].astype(int)
+            # relabel to get 'Describe why you came to GOV.UK today?'
+            df_feature = df_feature.rename(columns={"index": "q3"})
+            df_feature = df_feature.sort_values(by=["q3", "word_position"])
+            df_feature = df_feature.dropna(subset=[linguistic_feature], axis=0)
+
+            return df_feature
+
+    except Exception:
+        raise
--- tests/fixtures/fixture_helper_intent_uis.py
+++ tests/fixtures/fixture_helper_intent_uis.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 
 
 @pytest.fixture()
@@ -13,3 +14,10 @@ def text_pos_output():
             'dep': ['nsubj', 'aux', 'ROOT', 'prep', 'pcomp', 'compound', 'dobj', 'prep', 'quantmod', 'compound', 'pobj'],  # noqa: E501
             'lemma': ['Apple', 'be', 'look', 'at', 'buy', 'U.K.', 'startup', 'for', '$', '1', 'billion'],
             'stop': [False, True, False, True, False, False, False, True, False, False, False]}
+
+
+@pytest.fixture()
+def df_pos_output():
+    return pd.DataFrame(data={"q3": ["Apple is looking at buying U.K. startup for $1 billion"] * 11,
+                              "word_position": list(range(11)),
+                              "pos": ["PROPN", "AUX", "VERB", "ADP", "VERB", "PROPN", "NOUN", "ADP", "SYM", "NUM", "NUM"]})  # noqa: E501

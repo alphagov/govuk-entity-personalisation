2cccb1404e064839053e587f81b7907ca94dff4e
avisionh <a_vision@hotmail.co.uk>
2021-02-04 11:29:36 +0000

    Derive synonyms for verbs and objects
    This is so we can encapsulate more cases for what a user could enter.

--- notebooks/svo-synonyms/synoynms.py
+++ notebooks/svo-synonyms/synoynms.py
@@ -1,6 +1,6 @@
 import os
 import json
-import pandas as pd
+from gensim.models import Word2Vec
 
 
 DIR_INTERIM = os.getenv('DIR_DATA_INTERIM')
@@ -8,12 +8,23 @@ DIR_PROCESSED = os.getenv('DIR_DATA_PROCESSED')
 
 # load svo verbs and objects
 # from `src/make_features/extract_subject_verb_object_from_content_titles.py`
-with open(DIR_INTERIM + 'objects.json') as fp:
+with open(DIR_INTERIM + '/objects.json') as fp:
     key_objects = json.load(fp=fp)
-with open(DIR_INTERIM + 'verbs.json') as fp:
+with open(DIR_INTERIM + '/verbs.json') as fp:
     key_verbs = json.load(fp=fp)
 
-# load content store
-# from `notebooks/entities-synonyms/00_preprocess_content_store.py`
-df = pd.read_csv(filepath_or_buffer='data/processed/content_store_clean.csv',
-                 index_col=0)
+# load w2v model
+model_w2v = Word2Vec.load('model/word2vec_cbow.model')
+
+# transform each verb and object so suitable for comparing
+key_objects = [x.replace(' ', '_') for x in key_objects.keys()]
+key_verbs = [x.replace(' ', '_') for x in key_verbs.keys()]
+
+# get cbow terms
+cbow_terms = list(model_w2v.wv.vocab.keys())
+cbow_objects = list(set(key_objects) & set(cbow_terms))
+cbow_verbs = list(set(key_verbs) & set(cbow_terms))
+synonyms_objects = [model_w2v.wv.most_similar(positive=x) for x in cbow_objects]
+synonyms_verbs = [model_w2v.wv.most_similar(positive=x) for x in cbow_verbs]
+synonyms_objects = dict(zip(cbow_objects, synonyms_objects))
+synonyms_verbs = dict(zip(cbow_verbs, synonyms_verbs))

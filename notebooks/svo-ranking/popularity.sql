3436e543d880a93fb9585a9b721c65e1dc1751ab
avisionh <a_vision@hotmail.co.uk>
2021-02-08 16:50:13 +0000

    Remove pageTitle
    This is because it can change whilst pagePath can stay the same.

--- notebooks/svo-ranking/popularity.sql
+++ notebooks/svo-ranking/popularity.sql
@@ -1,11 +1,13 @@
 SELECT
     date
-    ,hits.page.pageTitle
     ,hits.page.pagePath
-    ,hits.page
+    ,COUNT(hits.page)
 FROM
     `govuk-bigquery-analytics.87773428.ga_sessions_intraday_*`,
     UNNEST(hits) AS hits
 WHERE
     _table_suffix > FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
-        AND hits.type = "PAGE";
+        AND hits.type = "PAGE"
+GROUP BY
+    date
+    ,hits.page.pagePath;

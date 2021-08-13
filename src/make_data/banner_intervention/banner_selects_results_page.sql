ab20ea706d0be3213068ffce7d6f946c266a8303
Alice Stephenson <alice.stephenson@digital.cabinet-office.gov.uk>
2021-08-13 09:20:28 +0100

    Query to determine the proportion of users that access the results page via the banner

---
+++ src/make_data/banner_intervention/banner_selects_results_page.sql
@@ -0,0 +1,44 @@
+/* This query extracts the total number and proportion of sessions that access the
+   checker via the banner intervention that access the results page
+*/
+
+
+DECLARE start_date STRING DEFAULT "20210803";
+DECLARE end_date STRING DEFAULT "20210816";
+
+CREATE OR REPLACE TABLE `govuk-bigquery-analytics.banner_intervention.banner_results_page` AS
+
+-- All distinct sessions that select the checker via the banner (eventAction =
+-- interventionClicked)
+
+WITH
+  sessions_select_banner AS (
+    SELECT DISTINCT
+      CONCAT(fullVisitorId, "-", visitId) AS sessionId
+    FROM `govuk-bigquery-analytics.87773428.ga_sessions_*`
+    CROSS JOIN UNNEST(hits) AS hits
+    WHERE _TABLE_SUFFIX BETWEEN start_date AND end_date
+      AND hits.eventInfo.eventAction = 'interventionClicked'
+  ),
+
+-- All distinct sessions that select the checker via the banner that also get to
+-- the end of the checker (i.e. the results page)
+
+  sessions_results_page AS (
+    SELECT DISTINCT
+      CONCAT(fullVisitorId, "-", visitId) AS sessionId
+    FROM `govuk-bigquery-analytics.87773428.ga_sessions_*`
+    CROSS JOIN UNNEST(hits) AS hits
+    WHERE _TABLE_SUFFIX BETWEEN start_date AND end_date
+      AND STARTS_WITH(hits.page.pagePath, "/next-steps-for-your-business/results")
+      AND CONCAT(fullVisitorId, "-", visitId) IN (SELECT sessionId FROM sessions_select_banner)
+  )
+
+-- The proportion of sessions that access the checker via the banner that also
+-- get to the end of the checker (i.e. the results page)
+
+SELECT
+  COUNT(DISTINCT sessionId) AS allSessionsThatSelectChecker,
+  (SELECT COUNT(DISTINCT sessionId) FROM sessions_results_page) AS allSessionsThatAccessResultsPage,
+  CAST (100 * (SELECT COUNT(DISTINCT sessionId) FROM sessions_results_page)/COUNT(DISTINCT sessionId) AS NUMERIC) AS proportionOfAllSessionsThatAccessResultsPage
+FROM sessions_select_banner

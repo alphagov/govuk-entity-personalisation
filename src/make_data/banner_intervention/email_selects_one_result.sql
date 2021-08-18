463bb062611b2c7985004ad9e57be25f7d0aa424
Alice Stephenson <alice.stephenson@digital.cabinet-office.gov.uk>
2021-08-18 13:21:35 +0100

    Refactored SQL to improve accuracy and readability

--- src/make_data/banner_intervention/email_selects_one_result.sql
+++ src/make_data/banner_intervention/email_selects_one_result.sql
@@ -29,8 +29,8 @@ WITH
     FROM `govuk-bigquery-analytics.87773428.ga_sessions_*`
     CROSS JOIN UNNEST(hits) AS hits
     WHERE _TABLE_SUFFIX BETWEEN start_date AND end_date
-      AND REGEXP_CONTAINS(hits.page.pagePath, r"^/run-business-from-home.*|^/renting-business-property-tenant-responsibilities.*|^/browse/business/premises-rates.*|^/running-a-limited-company/signs-stationery-and-promotional-material.*|^/business-finance-support?business_stages%5B%5D=not-yet-trading.*|^/business-coronavirus-support-finder.*|^/export-goods.*|^/research-export-markets.*|^/import-goods-into-uk.*|^/vat-registration.*|^/get-ready-to-employ-someone.*|^/browse/employing-people.*|^/corporation-tax.*|^/licence-finder.*|^https://www.abi.org.uk/products-and-issues/choosing-the-right-insurance/business-insurance/.*|^/check-if-you-need-tax-return.*|^/browse/business/sale-goods-services-data.*|^/cartels-price-fixing.*|^/data-protection-your-business.*|^https://www.hse.gov.uk/guidance/index.htm.*|^/browse/business/setting-up.*|^/running-a-limited-company/signs-stationery-and-promotional-material.*|^/managing-your-waste-an-overview.*|^/intellectual-property-an-overview.*|^/business-support-helpline.*|^/guidance/register-for-email-reminders-from-companies-house.*|^/vat-businesses.*")
-      AND CONCAT(fullVisitorId, "-", visitId) IN (SELECT sessionId FROM sessions_email_select)
+    AND hits.eventInfo.eventCategory = 'SmartAnswerClicked'
+    AND CONCAT(fullVisitorId, "-", visitId) IN (SELECT sessionId FROM sessions_email_select)
   )
 
 -- The proportion of sessions that select the checker via the email that also
